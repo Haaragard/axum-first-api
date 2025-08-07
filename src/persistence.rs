7fdb5fc9200c4e105d84fe041923337fe8ff19b2
haaragard <haaragard@gmail.com>
2025-08-06 21:35:57 -0300

    Refatorar SqliteRepository para incluir conexão com o banco de dados e criar tabela de usuários

--- src/persistence.rs
+++ src/persistence.rs
@@ -1,25 +1,64 @@
+use std::{fmt::Debug, sync::{Arc, Mutex}};
+
+use chrono::Utc;
+use rusqlite::Connection;
+
 use crate::{entity::user::User, error::{Error, Result}};
 
 #[derive(Clone, Debug)]
-pub struct SqliteRepository {}
+pub struct SqliteRepository {
+    conn: Arc<Mutex<Connection>>,
+}
 impl SqliteRepository {
-    pub fn new(_path: String) -> Self {
-        Self {}
+    pub fn new(path: String) -> Result<Self, Error> {
+        let conn = Arc::new(Mutex::new({
+            match path.eq(":memory") {
+                true => Connection::open_in_memory().expect("Could not open SQLite database."),
+                false => Connection::open(path)?
+            }
+        }));
+
+        let instance = Self { conn };
+        instance.create_table()?;
+
+        Ok(instance)
+    }
+
+    pub fn create_table(&self) -> Result<(), Error> {
+        if let Ok(conn) = self.conn.lock() {
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS users (
+                    id          TEXT PRIMARY KEY,
+                    name        TEXT NOT NULL,
+                    created_at  DATETIME NOT NULL
+                )",
+                []
+            )?;
+
+            return Ok(());
+        }
+
+        Err(Error::ConnectionLockError)
     }
 
     pub fn create_user(&self, input: CreateUserInputDTO) -> Result<CreateUserOutputDTO, Error> {
-        let user = User::new(
-            Some(guid_create::GUID::rand().to_string()),
-            input.name,
-        );
-        if user.id.is_none() {
-            return Err(Error::UserCreateCouldNotCreateUser);
-        }
+        let user_guid = guid_create::GUID::rand().to_string();
+        let created_at = Utc::now().to_rfc3339();
 
-        Ok(CreateUserOutputDTO {
-            id: user.id.unwrap(),
-            name: user.name,
-        })
+        let result = self.conn.lock().unwrap().execute(
+            "INSERT INTO users (id, name, created_at) VALUES (?1, ?2, ?3)",
+            (&user_guid, &input.name, &created_at)
+        );
+
+        match result {
+            Ok(_) => Ok(CreateUserOutputDTO {
+                user: User::new(
+                    Some(user_guid),
+                    input.name,
+                )
+            }),
+            _ => Err(Error::UserCreateCouldNotCreateUser)
+        }
     }
 }
 
@@ -28,6 +67,5 @@ pub struct CreateUserInputDTO {
 }
 
 pub struct CreateUserOutputDTO {
-    pub id: String,
-    pub name: String,
+    pub user: User,
 }

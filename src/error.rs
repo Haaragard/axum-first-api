5cfaef8e96df2aa62a4f09e3c50f2a62a0a76632
haaragard <haaragard@gmail.com>
2025-08-06 21:35:46 -0300

    Adicionar novos tipos de erro ao enum Error para melhor tratamento de falhas

--- src/error.rs
+++ src/error.rs
@@ -7,14 +7,22 @@ pub enum Error {
     // -- User
     UserCreateCouldNotCreateUser,
 
+    // -- Repository
+    ConnectionLockError,
+
+    #[from]
+    #[warn(dead_code)]
+    RusqliteError(rusqlite::Error),
+
     // -- Externals
     #[from]
+    #[warn(dead_code)]
     Io(std::io::Error),
 }
 
 impl std::fmt::Display for Error {
-    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(fmt, "{self:?}")
+    fn fmt(&self, format: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(format, "{self:?}")
     }
 }
 
